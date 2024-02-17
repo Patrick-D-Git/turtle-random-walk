1ae450efc875a3d449f2446eb34a7b8244b2075c
Patrick D <patrick.montejo@gmail.com>
2024-02-17 14:33:10 -0500

    added option to continue random walk

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from turtle import Turtle, Screen
 import random
 
+
+def random_walk():
+
+    for _ in range(50):
+        # tim will do a random walk
+        random_direction = random.choice(list(direction.values()))  # picks a random direction
+        random_color = random.choice(colors)  # picks a random color
+        tim.pencolor(random_color)
+        tim.setheading(random_direction)
+        tim.forward(50)  # tim moves forward
+
+
 tim = Turtle()
 screen = Screen()
 tim.pensize(10)
@@ -15,14 +27,14 @@ direction = {
     "south": 270,
 }
 
+continue_random_walk = True
 
-for _ in range(50):
-    # tim will do a random walk
-    random_direction = random.choice(list(direction.values()))  # picks a random direction
-    random_color = random.choice(colors)  # picks a random color
-    tim.pencolor(random_color)
-    tim.setheading(random_direction)
-    tim.forward(50)  # tim moves forward
+while continue_random_walk:
 
+    random_walk()
+
+    if input("Do you want to continue? Y/N: ").upper() == "N":
+        continue_random_walk = False
+        print("Exiting Application..")
 
 screen.exitonclick()
